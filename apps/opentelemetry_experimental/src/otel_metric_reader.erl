d43403d28a3f7e0a03c57c1d70a1d3b64d7470cf
Alberto Sartori <alberto.sartori.as@gmail.com>
2023-08-29 17:49:52 +0200

    Reader setup with httpd server

--- apps/opentelemetry_experimental/src/otel_metric_reader.erl
+++ apps/opentelemetry_experimental/src/otel_metric_reader.erl
@@ -26,7 +26,8 @@
 
 -export([start_link/3,
          collect/1,
-         shutdown/1]).
+         shutdown/1,
+         collect_/4]).
 
 -export([init/1,
          handle_call/3,
@@ -64,7 +65,7 @@ start_link(ReaderId, ProviderSup, Config) ->
     gen_server:start_link(?MODULE, [ReaderId, ProviderSup, Config], []).
 
 collect(ReaderPid) ->
-    ReaderPid ! collect.
+    gen_server:call(ReaderPid, collect).
 
 shutdown(ReaderPid) ->
     gen_server:call(ReaderPid, shutdown).
@@ -111,62 +112,44 @@ handle_continue(register_with_server, State=#state{provider_sup=ProviderSup,
 
 handle_call(shutdown, _From, State) ->
     {reply, ok, State};
-handle_call(_, _From, State) ->
-    {noreply, State}.
-
-handle_cast(_, State) ->
-    {noreply, State}.
-
-handle_info(collect, State=#state{exporter=undefined,
-                                  export_interval_ms=ExporterIntervalMs,
-                                  tref=TRef}) when TRef =/= undefined andalso
-                                                   ExporterIntervalMs =/= undefined ->
-    erlang:cancel_timer(TRef, [{async, true}]),
-    NewTRef = erlang:send_after(ExporterIntervalMs, self(), collect),
-    {noreply, State#state{tref=NewTRef}};
-handle_info(collect, State=#state{id=ReaderId,
-                                  exporter={ExporterModule, Config},
-                                  export_interval_ms=undefined,
-                                  tref=undefined,
+handle_call(collect, _From, State=#state{id=ReaderId,
+                                  exporter=Exporter,
                                   callbacks_tab=CallbacksTab,
                                   view_aggregation_tab=ViewAggregationTab,
                                   metrics_tab=MetricsTab,
                                   resource=Resource
                                  }) ->
-    %% collect from view aggregations table and then export
-    Metrics = collect_(CallbacksTab, ViewAggregationTab, MetricsTab, ReaderId),
+    TRef = update_timer(State#state.tref, State#state.export_interval_ms),
+    Reply = collect_and_export(ReaderId, Exporter, CallbacksTab, ViewAggregationTab, MetricsTab, Resource),
+    {reply, Reply, State#state{tref=TRef}};
+handle_call(_, _From, State) ->
+    {noreply, State}.
 
-    otel_exporter:export_metrics(ExporterModule, Metrics, Resource, Config),
-
-    {noreply, State};
-handle_info(collect, State=#state{id=ReaderId,
-                                  exporter={ExporterModule, Config},
-                                  export_interval_ms=ExporterIntervalMs,
-                                  tref=TRef,
-                                  callbacks_tab=CallbacksTab,
-                                  view_aggregation_tab=ViewAggregationTab,
-                                  metrics_tab=MetricsTab,
-                                  resource=Resource
-                                 }) when TRef =/= undefined andalso
-                                         ExporterIntervalMs =/= undefined  ->
-    erlang:cancel_timer(TRef, [{async, true}]),
-    NewTRef = erlang:send_after(ExporterIntervalMs, self(), collect),
-
-    %% collect from view aggregations table and then export
-    Metrics = collect_(CallbacksTab, ViewAggregationTab, MetricsTab, ReaderId),
-
-
-    otel_exporter:export_metrics(ExporterModule, Metrics, Resource, Config),
-
-    {noreply, State#state{tref=NewTRef}};
-%% no tref or exporter, do nothing at all
+handle_info(collect, State) ->
+    {reply, _, NewState} = handle_call(collect, undefined, State),
+    {noreply, NewState};
 handle_info(_, State) ->
+    {noreply, State}.
+
+handle_cast(_, State) ->
     {noreply, State}.
 
 code_change(State) ->
     {ok, State}.
 
 %%
+collect_and_export(_ReaderId, undefined, _CallbacksTab, _ViewAggregationTab, _MetricsTab, _Resource) ->
+    ok;
+collect_and_export(ReaderId, {ExporterModule, Config}, CallbacksTab, ViewAggregationTab, MetricsTab, Resource) ->
+    %% collect from view aggregations table and then export
+    Metrics = collect_(CallbacksTab, ViewAggregationTab, MetricsTab, ReaderId),
+    otel_exporter:export_metrics(ExporterModule, Metrics, Resource, Config).
+
+update_timer(undefined, undefined) ->
+    undefined;
+update_timer(TRef, ExporterIntervalMs) ->
+    erlang:cancel_timer(TRef, [{async, true}]),
+    erlang:send_after(ExporterIntervalMs, self(), collect).
 
 -spec collect_(any(), ets:table(), any(), reference()) -> [any()].
 collect_(CallbacksTab, ViewAggregationTab, MetricsTab, ReaderId) ->
